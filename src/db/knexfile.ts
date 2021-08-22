60f2d19e1bd9d1ca68a16029e853114d1c54d6f2
hannupekka <hannupekka@gmail.com>
2021-08-22 11:49:15 +0300

    Update pg

--- src/db/knexfile.ts
+++ src/db/knexfile.ts
@@ -6,6 +6,7 @@ module.exports = {
     min: 2,
     max: 10,
   },
+  ssl: { rejectUnauthorized: false },
   connection: `${config.DATABASE_URL}?ssl=true`,
   migrations: {
     directory: 'migrations',
